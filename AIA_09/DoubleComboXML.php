dc860d4014438a336f42759920a4429f532dadb6
zhouleilei <leileizhou1981@163.com>
2015-01-13 22:22:23 +0800

    sample of chapter 9 of ajax in action

---
+++ AIA_09/DoubleComboXML.php
@@ -0,0 +1,23 @@
+/**
+ * Created by PhpStorm.
+ * User: ryan
+ * Date: 2015/1/13
+ * Time: 21:31
+ */
+<?php
+echo '<?xml version="1.0" ?>
+<selectChoice>
+  <selectElement>
+    <formName>Form1</formName>
+    <formElem>ddlTerritory</formElem>
+  </selectElement>
+  <entry>
+    <optionText>Select A Territory</optionText>
+    <optionValue>-1</optionValue>
+  </entry>
+  <entry>
+    <optionText>TerritoryDescription</optionText>
+    <optionValue>TerritoryID</optionValue>
+  </entry>
+</selectChoice>';
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    phpinfo();
+?>
+/**
+ * Created by PhpStorm.
+ * User: ryan
+ * Date: 2015/1/13
+ * Time: 21:29
+ */
